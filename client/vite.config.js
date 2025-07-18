6b46dabcd688be67d04585d635d1a04c9e455eea
Singhoustic <singhgurpreet1013@gmail.com>
2025-07-19 00:00:48 +0530

    vite file updated.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,24 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// import path from "path"
+// import tailwindcss from "@tailwindcss/vite"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+// import sitemap from 'vite-plugin-sitemap';
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +26,19 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    fs: {
+      allow: [".."],
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"), // ✅ Correct relative path
+    },
+    copyPublicDir: true,
+    outDir: "dist",
+  },
+  // optimizeDeps: {
+  //   exclude: ["blogs/*"],
+  // },
+});
